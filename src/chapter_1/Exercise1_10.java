abeec8f53fb9c1bef59a2cfb2b5ab5821db8afd6
William Pennington <odielag@gmail.com>
2016-02-12 10:02:15 -0800

    finished exercise 1.10

--- src/chapter_1/Exercise1_10.java
+++ src/chapter_1/Exercise1_10.java
@@ -1,10 +1,41 @@
 package chapter_1;
 
 public class Exercise1_10 {
+	
+	/*
+	 * (Average speed in miles) Assume a runner runs 14 kilometers in 45 minutes and 30
+seconds. Write a program that displays the average speed in miles per hour. (Note
+that 1 mile is 1.6 kilometers.)
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//test//		System.out.println(convertmphtokph(20));
+//test//		System.out.println(convertkphtomph(20));
+		//converting kilometers per minutes to kph
+		double kph = convertkpmtokph(14, 45.5);
+		double mph = convertkphtomph(kph);
+		
+		
+		System.out.printf("The runner runs about %f mph.\n", mph);
+	}
+	
+	public static double convertmphtokph (double mph){
+		double kph;
+		kph = mph * 1.6;
+		return kph;
+	}
+	
+	public static double convertkphtomph (double kph){
+		double mph;
+		mph = kph / 1.6;
+		return mph;
+	}
+	
+	public static double convertkpmtokph (double km, double minute){
+		double kph = km / (minute / 60);
+		return kph;
 	}
 
 }
+
+//finished//
