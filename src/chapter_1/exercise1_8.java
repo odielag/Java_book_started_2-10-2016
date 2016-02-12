50955c5336c638bc77cc55c8a5bdb0b532e14ff3
William Pennington <odielag@gmail.com>
2016-02-12 08:30:35 -0800

    tiny bit more progress

---
+++ src/chapter_1/exercise1_8.java
@@ -0,0 +1,17 @@
+package chapter_1;
+
+public class exercise1_8 {
+
+	/*
+	 * (Area and perimeter of a circle) Write a program that displays the area and perim-
+eter of a circle that has a radius of 5.5 using the following formula:
+perimeter = 2 * radius * pi
+area = radius * radius * pi
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
