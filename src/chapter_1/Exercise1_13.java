f2a1ecad07c117e6625f3ff031f3dcbdf238cfad
William Pennington <odielag@gmail.com>
2016-02-13 20:57:30 -0800

    setup new exercise: 1.13

---
+++ src/chapter_1/Exercise1_13.java
@@ -0,0 +1,32 @@
+package chapter_1;
+
+public class Exercise1_13 {
+	
+	/*
+	 * (Algebra: solve 2 * 2 linear equations) You can use Cramer’s rule to solve the fol-
+lowing 2 * 2 system of linear equation:
+
+ax + by = e
+
+cx + dy = f		
+
+     ed - bf
+x = ---------
+     ad - bc
+
+     af - ec
+y = ---------
+	 ad - bc
+
+
+Write a program that solves the following equation and displays the value for x and y :
+3.4x + 50.2y = 44.5
+2.1x + .55y = 5.9
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
