f71ba056a3ab933e546f459828921d3daa18b92b
William Pennington <odielag@gmail.com>
2016-02-13 20:44:21 -0800

    modified print function's output

--- src/chapter_1/Exercise1_12.java
+++ src/chapter_1/Exercise1_12.java
@@ -9,29 +9,28 @@ hour. (Note that 1 mile is 1.6 kilometers.)
 	 */
 
 	public static void main(String[] args) {
-		System.out.printf("The runner is running at " +
-				(convertmphtokph
-						(convertMilesYearsDaysHoursMinutesSecondsMilliseconds_ToMilesPerHour(
-								24, 0, 0, 1, 40, 0, 0)
-								)
-						+ " Kilometers per hour."));
+		System.out.printf("The runner is running at %.4f Kilometers per hour.",	
+							(convertmphtokph
+								(convertMilesPerTime(24, 0, 0, 1, 40, 0, 0))
+							)
+						 );
 
 	}
 
-	public static double convertMilesYearsDaysHoursMinutesSecondsMilliseconds_ToMilesPerHour(
+	public static double convertMilesPerTime(
 			double miles, double years, double days, double hours, double minutes, double seconds, double ms){
 		return (miles / 
-				(years * 365 * 24 +
+				(years * 365 * 24.00 +
 						days * 24 +
 						hours +
-						minutes / 60 +
+						minutes / 60.00 +
 						seconds / 3600 +
 						ms / 3600000));
 	}
 
 	public static double convertmphtokph (double mph){
 		double kph;
-		kph = mph * 1.6;
+		kph = mph * 1.60;
 		return kph;
 	}
 
@@ -39,4 +38,4 @@ hour. (Note that 1 mile is 1.6 kilometers.)
 }
 
 //current result:
-//The runner is running at 23.040000000000006 Kilometers per hour.
+//The runner is running at 23.0400 Kilometers per hour.
