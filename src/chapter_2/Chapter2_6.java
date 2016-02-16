669ead38877cc530763d04cbee0b169a105d7431
William Pennington <odielag@gmail.com>
2016-02-16 03:19:28 -0800

    reading chapter 2 and practicing/testing

---
+++ src/chapter_2/Chapter2_6.java
@@ -0,0 +1,35 @@
+package chapter_2;
+
+public class Chapter2_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		//testing
+		double i;
+		double j;
+		double k;
+		i = j = k = 1;
+		System.out.printf(i + "\n" + j + "\n" + k);
+		*/
+		
+		/*
+		//testing 2.6
+		int j;
+		int k;
+		int i = j = k = 2;
+		System.out.println(i + " " + j + " " + k);
+		*/
+
+		//System.out.printf(1.23456E2 + "\n");
+		//Result:
+		//123.456
+		
+		//System.out.printf(123.456E2 + "\n");
+		//Result:
+		//12345.6
+
+	}
+
+}
