f6931ca21a1262dbbc5af5e334c807fc96873060
William Pennington <odielag@gmail.com>
2016-02-16 21:47:26 -0800

    slowly making progress reading chapter 2

--- src/chapter_2/Chapter2_22.java
+++ src/chapter_2/Chapter2_22.java
@@ -7,6 +7,19 @@ public class Chapter2_22 {
 		//System.out.println( 4 / (3*(r + 34)) - 9*(a + bc) + (3+d*(2+a))/(a + b*d));
 		//System.out.println(5.5 * (Math.pow((r + 2.5), (2.5+t));
 
+		//2.12
+		long totalMilliseconds = System.currentTimeMillis(); //1 from pg52
+		long totalSeconds = totalMilliseconds / 1000; //2
+		long currentSecond = totalSeconds % 60; //3
+		long totalMinutes = totalSeconds / 60; //4
+		long currentMinute = totalMinutes % 60; //5
+		long totalHours = totalMinutes / 60; //6
+		long currentHour = totalHours % 24; //7
+		
+		// Display results
+		System.out.println("Current time is " + currentHour + ":" + 
+		currentMinute + ":" + currentSecond + " GMT");
+		
 	}
 
 }
