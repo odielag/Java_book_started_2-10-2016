75f8d5086adfd6890404b956b55e1c3d73aa5dd5
William Pennington <odielag@gmail.com>
2016-02-18 23:10:19 -0800

    some progress

---
+++ src/chapter_2/InChapter_2_17__ChangingMoney.java
@@ -0,0 +1,29 @@
+package chapter_2;
+
+public class InChapter_2_17__ChangingMoney {
+
+	/*
+Suppose you want to develop a program that changes a given amount of money into smaller
+monetary units. The program lets the user enter an amount as a double value representing a
+total in dollars and cents, and outputs a report listing the monetary equivalent in the maximum
+number of dollars, quarters, dimes, nickels, and pennies, in this order, to result in the mini-
+mum number of coins.
+	 */
+	
+	public static void main(String[] args) {
+		double amount; //The original amount of money in *.?? format
+		long dollarBill_100;
+		long dollarBill_50;
+		long dollarBill_20;
+		long dollarBill_10;
+		long dollarBill_5;
+		long dollarBill_1;
+		//testing// System.out.println(Byte.MAX_VALUE);
+		byte quarter;
+		byte dime;
+		byte nickel;
+		byte penny;
+		
+	}
+
+}
