f835d98ad301a88b08328de5fd9ea962349535bb
William Pennington <odielag@gmail.com>
2016-02-19 23:48:04 -0800

    closing down for the night

---
+++ src/testing/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author username
+ *
+ */
+package testing;
---
+++ src/testing/testing.java
@@ -0,0 +1,24 @@
+package testing;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//System.out.println(Integer.MAX_VALUE);
+		
+	    //int n = 10000 * 10000 * 10000;
+	    //System.out.println("n is " + n);
+		
+		//float x = 1.0f;
+		//System.out.println(x);
+		/*
+In Java, every floating-point number (any number with a decimal point) 
+defaults to a double, which is more precise than a float. And by default, 
+Java does not let you convert a double to a float because of the loss 
+of precision.
+		 */
+		
+	}
+	
+}
