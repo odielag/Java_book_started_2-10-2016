579b741849a3886081b1e434eea5b11fae49678f
William Pennington <odielag@gmail.com>
2016-02-20 18:22:04 -0800

    test

---
+++ src/chapter_2/Exercise2_1__ConvertCelsiusToFahrenheit.java
@@ -0,0 +1,19 @@
+package chapter_2;
+
+public class Exercise2_1__ConvertCelsiusToFahrenheit {
+	
+	/*
+(Convert Celsius to Fahrenheit) Write a program that reads a Celsius degree in
+a double value from the console, then converts it to Fahrenheit and displays the
+result. The formula for the conversion is as follows:
+fahrenheit = (9 / 5) * celsius + 32
+Hint: In Java, 9 / 5 is 1 , but 9.0 / 5 is 1.8 .
+Here is a sample run:
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
