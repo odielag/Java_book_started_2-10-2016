ffafd8eb9b901b054e8be26724f87c85734151fc
William Pennington <odielag@gmail.com>
2016-02-22 02:26:51 -0800

    Create README.md testing how to push or merge github...

---
+++ src/chapter_2/Chapter2_6__Testings.java
@@ -0,0 +1,43 @@
+package chapter_2;
+
+public class Chapter2_6__Testings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//testingetBeansProjecetBeansProjectsetBeansProjectsts
+		/*
+                double i;
+		double j;
+		double k;
+		i = j = k = 1;
+		System.out.printf(i + "\n" + j + "\n" + k);
+		*/
+
+		/*
+		//testing 2.6
+		int j;
+		int k;
+		int i = j = k = 2;
+		System.out.println(i + " " + j + " " + k);
+		 */
+
+		//System.out.printf(1.23456E2 + "\n");
+		//Result:
+		//123.456
+
+		//System.out.printf(123.456E2 + "\n");
+		//Result:
+		//12345.6
+
+		//System.out.printf(123_456 + "\n");
+		//Result:
+		//123456
+
+		//System.out.printf(123_456e12 + "\n");
+		//Result:
+		//1.23456E17
+	}
+
+}
