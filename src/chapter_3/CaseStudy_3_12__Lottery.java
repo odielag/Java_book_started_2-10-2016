d021bc7232d9a79d460fcc753c55f4e9a9eca3b0
William Pennington <odielag@gmail.com>
2016-03-03 13:31:46 -0800

    finished a case study

--- src/chapter_3/CaseStudy_3_12__Lottery.java
+++ src/chapter_3/CaseStudy_3_12__Lottery.java
@@ -1,7 +1,7 @@
 package chapter_3;
 
 public class CaseStudy_3_12__Lottery {
-	
+
 	/*
 Suppose you want to develop a program to play lottery. The program randomly generates a
 lottery of a two-digit number, prompts the user to enter a two-digit number, and determines
@@ -15,8 +15,61 @@ as 08 and number 0 is treated as 00 in the program. Listing 3.8 gives the comple
 	 */
 
 	public static void main(String[] args) {
+
+		// input
+		int randomNumber;
+		String stringRandomNumber;
+		int lotteryPick;
+		String stringLotteryPick;
+		String output = "";
+		char randomNumber0;
+		char randomNumber1;
+		char lotteryPick0;
+		char lotteryPick1;
 		
+			// generate a two-digit random number
+		randomNumber = (int)(Math.random() * 100);
+		stringRandomNumber = Integer.toString(randomNumber);
+		if (randomNumber / 10 == 0){
+			stringRandomNumber = String.format("0%s", Integer.toString(randomNumber));
+		}
+
+			//ex.Enter your lottery pick (two digits): 15
+		System.out.printf("Enter your lottery pick (two digits): ");
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		lotteryPick = input.nextInt();
+		input.close();
 		
+		// processing
+		stringLotteryPick = Integer.toString(lotteryPick);
+		if (lotteryPick / 10 == 0){
+			stringLotteryPick = String.format("0%s", Integer.toString(lotteryPick));
+		}
+		
+		randomNumber0 = (char)(stringRandomNumber.charAt(0));
+		randomNumber1 = (char)(stringRandomNumber.charAt(1));
+		lotteryPick0 = (char)(stringLotteryPick.charAt(0));
+		lotteryPick1 = (char)(stringLotteryPick.charAt(1));
+
+		if (randomNumber0 == lotteryPick0 && randomNumber1 == lotteryPick1){
+			output = "Exact match: you win $10,000";
+		}
+		else if (randomNumber0 == lotteryPick1 && randomNumber1 == lotteryPick0){
+			output = "Match all digits: you win $3,000";
+		}
+		else if (randomNumber0 == lotteryPick0 || randomNumber0 == lotteryPick1
+				|| randomNumber1 == lotteryPick0 || randomNumber1 == lotteryPick1){
+			output = "Match one digit: you win $1,000";
+		}
+				
+		// output
+		System.out.printf("The lottery number is: %s\n", stringRandomNumber);
+		System.out.printf("%s", output);
+
+		/*
+			The lottery number is 15
+			Exact match: you win $10,000
+		 */
 
 	}
 
