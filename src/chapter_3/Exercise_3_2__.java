f48a5c8dd43ea0992593cc174d5122e09330feb6
William Pennington <odielag@gmail.com>
2016-03-06 18:38:43 -0800

    exercise 3.2 completed.

--- src/chapter_3/Exercise_3_2__.java
+++ src/chapter_3/Exercise_3_2__.java
@@ -1,5 +1,7 @@
 package chapter_3;
 
+import java.util.Scanner;
+
 public class Exercise_3_2__ {
 	
 	/*
@@ -8,10 +10,50 @@ erates two integers and prompts the user to enter the sum of these two integers.
 Revise the program to generate three single-digit integers and prompt the user to
 enter the sum of these three integers.
 	 */
+	
+	/* 3.1 code...
+	public static void main(String[] args) {
+		
+		int number1 = (int)(System.currentTimeMillis() % 10);
+		int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+		
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print(
+				"What is " + number1 + " + " + number2 + "? ");
+
+		int number = input.nextInt();
+		
+		System.out.println(
+				number1 + " + " + number2 + " = " + number + " is " +
+		(number1 + number2 == number));
+		
+	}
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int number1 = (int)(System.currentTimeMillis() % 10);
+		int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+		int number3 = (int)(System.currentTimeMillis() / 5 % 10);
+		
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print(
+				"What is " + number1 + " + " + number2 + " + " + number3 + " ? ");
 
+		int number = input.nextInt();
+		
+		input.close();
+		
+		System.out.println(
+				number1 + " + " + number2 + " + " + number3 + " = " + number + " is " +
+		(number1 + number2 + number3 == number));
+		
 	}
 
 }
+
+// Finished by William ODieLag Pennington on 3/6/2016; 
